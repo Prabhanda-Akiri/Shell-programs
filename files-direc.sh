7e0ecb4930b85a1e56c417b4a4e12952c61a1880
Akiri Prabhanda <31247199+Sahithi-Akiri@users.noreply.github.com>
2017-10-03 19:17:48 +0530

    counts,displays files and directories

--- files-direc.sh
+++ files-direc.sh
@@ -7,17 +7,11 @@ ls -d */
 
 c=0
 
-f=`ls *.sh | wc -w`
-
-c=`expr $c + $f`
-
-f=`ls *.txt | wc -w`
-
+f=`ls *.* | wc -w`
 c=`expr $c + $f`
 
 
 
 echo "the total number of files are:	$c"
 echo "the files are:"
-ls *.sh
-ls *.txt
+ls *.*
