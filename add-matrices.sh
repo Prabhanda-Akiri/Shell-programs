c34f82621967d41e32fcbcc0cac635117d0feb66
Akiri Prabhanda <31247199+Sahithi-Akiri@users.noreply.github.com>
2017-10-09 12:24:44 +0530

    adds two matrices

--- add-matrices.sh
+++ add-matrices.sh
@@ -3,25 +3,26 @@
 echo "enter the dimensions of the matrix:"
 read m
 read n
-echo "enter the first matrix:"
+echo "the first matrix:"
 for((i=0;i<m;i++))
 do
 	for((j=0;j<n;j++))
 	do
-		read p
-		a[i,j]=$p
+		a[i,j]=1
+		echo -n "${a[$i,$j]} "
 	done
-
+	echo " "
 done
 
-echo "enter the second matrix:"
+echo "the second matrix:"
 for((i=0;i<m;i++))
 do
 	for((j=0;j<n;j++))
 	do
-		read p
-		b[i,j]=$p
+		b[i,j]=2
+		echo -n "${b[$i,$j]} "
 	done
+	echo ""
 done
 
 echo "The added matrix is:"
